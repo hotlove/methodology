ae976a39346c4f36494e928daac0014bccb72a18
guolejian <guolejian@126.com>
2020-08-05 11:02:28 +0800

    first commit

---
+++ src/main/java/com/guo/methodology/MethodologyApplication.java
@@ -0,0 +1,13 @@
+package com.guo.methodology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MethodologyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MethodologyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/guo/methodology/MethodologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guo.methodology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MethodologyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
